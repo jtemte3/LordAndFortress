fadc9acd77b8ea7b8e66e7f3d7905d641b0f8619
jtemte3 <47343054+jtemte3@users.noreply.github.com>
2024-02-29 16:02:16 -0600

    Weapon Update: - Model Changes     - Rigged Custom Troop Model     - Created Inverse Kinematic rig and constraints 	- Added Basic Idle animations 	- Rigged all current armor, shield and weapon objects     - Weapons and Shields are now to scale with other objects 	- Tabbard is now integrated with the torso, instead of overlaying on top - Faction Editor screen     - Added Weapon and Shield customization     - Added basic idle animations     - Added ability  to rotate and center your custom troop preview 	- re-wrote the armor editing, saving and loading functionality to handle the new armor system - Main Menu Screen     - Added the Custom Troop and a stand-in Custom Hero to the main menu 	- Wrote a script to load any custom troops from the faction, and delete objects that are not in use - General 	- Moved the Custom Troop Model from a blender file to an FBX that allows importing of separate animations     - Removed outdated weapon and shield models

---
+++ Assets/FactionCustomizer/CustomUnitLoader.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomUnitLoader : MonoBehaviour
+{
+    public List<GameObject> helmets = new();
+    public List<Material> baseArmorMaterials = new();
+    public List<GameObject> armors = new();
+    public List<Material> legMaterials = new();
+    public List<GameObject> weapons = new();
+    public List<GameObject> shields = new();
+    public GameObject legs;
+
+    public void LoadUnit(CustomUnitObject newUnit)
+    {
+        for (int i = 0; i < helmets.Count; i++)
+        {
+            if (newUnit.helmetId.Equals(i))
+            {
+                helmets[i].SetActive(true);
+            }
+            else
+            {
+                Destroy(helmets[i]);
+            }
+        }
+
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (newUnit.weaponId.Equals(i))
+            {
+                weapons[i].SetActive(true);
+            }
+            else
+            {
+                Destroy(weapons[i]);
+            }
+        }
+
+        for (int i = 0; i < shields.Count; i++)
+        {
+            if (newUnit.shieldId.Equals(i))
+            {
+                shields[i].SetActive(true);
+            }
+            else
+            {
+                Destroy(shields[i]);
+            }
+        }
+
+        for (int i = 0; i < armors.Count; i++)
+        {
+            if (newUnit.armorId.Equals(i))
+            {
+                armors[i].SetActive(true);
+            }
+            else
+            {
+                Destroy(armors[i]);
+            }
+        }
+
+        var armorMats = armors[newUnit.armorId].GetComponent<Renderer>().sharedMaterials;
+        for (int i = 0; i < armorMats.Length; i++)
+        {
+            if (armorMats[i].name.Contains("Cloth") && !armorMats[i].name.Contains("Tabbard"))
+            {
+                armorMats[i] = baseArmorMaterials[newUnit.baseArmorId];
+            }
+            if (armorMats[i].name.Contains("ChainMail"))
+            {
+                armorMats[i] = baseArmorMaterials[newUnit.baseArmorId];
+            }
+        }
+        armors[newUnit.armorId].GetComponent<Renderer>().sharedMaterials = armorMats;
+
+        var legMats = legs.GetComponent<Renderer>().sharedMaterials;
+        legMats[1] = legMaterials[newUnit.legsId];
+
+        legs.GetComponent<Renderer>().sharedMaterials = legMats;
+        legs.SetActive(true);
+
+    }
+
+}
--- Assets/FactionCustomizer/FactionManager.cs
+++ Assets/FactionCustomizer/FactionManager.cs
@@ -12,6 +12,10 @@ public class FactionManager : MonoBehaviour
     public List<Color> FactonColors = new();
     public Color currentColor;
     public string factionJsonPath;
+    public bool loadFromFile = true;
+    public Transform customTroopPreviewTransform;
+    public float speed = 0f;
+    public Animator controller;
 
     // Start is called before the first frame update
     void Start()
@@ -23,41 +27,85 @@ public class FactionManager : MonoBehaviour
         unitCustomization.UpdateColor(currentColor);
     }
 
+    private void Update()
+    {
+        customTroopPreviewTransform.Rotate(Vector3.up, Time.deltaTime * speed);
+    }
+
     public void SetFactionColor(int position)
     {
         currentColor = FactonColors[position];
         unitCustomization.UpdateColor(currentColor);
     }
 
+    public void RotateCharacter(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public void ResetCharacterRotation()
+    {
+        customTroopPreviewTransform.rotation = Quaternion.Euler(0, 180, 0);
+        speed = 0;
+    }
+
+    public void ChangeCharacterPose()
+    {
+        if (controller.GetBool("Ready").Equals(true))
+        {
+            controller.SetBool("Ready", false);
+        }
+        else
+        {
+            controller.SetBool("Ready", true);
+        }
+    }
+
     public void SaveFactionDetails()
     {
-        StreamWriter writer = new StreamWriter(factionJsonPath);
+        if (loadFromFile)
+        {
+            StreamWriter writer = new StreamWriter(factionJsonPath);
 
-        CustomFactionObject newFaction = new();
-        newFaction.name = factionName.text;
-        newFaction.color = currentColor;
-        newFaction.customUnits = new List<CustomUnitObject>();
-        newFaction.customUnits.Add(unitCustomization.ExportUnit());
+            CustomFactionObject newFaction = new();
+            newFaction.name = factionName.text;
+            newFaction.color = currentColor;
+            newFaction.customUnits = new List<CustomUnitObject>();
+            newFaction.customUnits.Add(unitCustomization.ExportUnit());
 
-        string factionJson = JsonUtility.ToJson(newFaction, true);
+            string factionJson = JsonUtility.ToJson(newFaction, true);
 
-        writer.Write(factionJson);
-        writer.Flush();
-        writer.Close();
+            writer.Write(factionJson);
+            writer.Flush();
+            writer.Close();
+        }
+        else
+        {
+            Debug.Log("Mocking Save Function");
+        }
+        
     }
 
     public void LoadFactionDetails()
     {
-        StreamReader reader = new(factionJsonPath);
-        string configJson = reader.ReadToEnd();
+        if (loadFromFile)
+        {
+            StreamReader reader = new(factionJsonPath);
+            string configJson = reader.ReadToEnd();
 
-        CustomFactionObject loadedFaction = JsonUtility.FromJson<CustomFactionObject>(configJson);
-        factionName.text = loadedFaction.name;
-        currentColor = loadedFaction.color;
-        unitCustomization.LoadUnit(loadedFaction.customUnits[0]);
-        unitCustomization.UpdateColor(currentColor);
+            CustomFactionObject loadedFaction = JsonUtility.FromJson<CustomFactionObject>(configJson);
+            factionName.text = loadedFaction.name;
+            currentColor = loadedFaction.color;
+            unitCustomization.LoadUnit(loadedFaction.customUnits[0]);
+            unitCustomization.UpdateColor(loadedFaction.color);
 
-        reader.Close();
+            reader.Close();
+        }
+        else
+        {
+            Debug.Log("Mocking Load Function");
+        }
+
     }
 
 }
