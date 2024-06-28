9e5c809e1ed7ebd71f16bfe72cc53d7c995353ce
KittKett <kittiesandkettles@gmail.com>
2024-06-28 16:57:57 -0500

    Handling edge cases

--- Assets/FactionCustomizer/FactionManager.cs
+++ Assets/FactionCustomizer/FactionManager.cs
@@ -58,6 +58,16 @@ public class FactionManager : MonoBehaviour
 
     private void moveColorIndicator(int position)
     {
+        //handle -1 position (edge case)
+        if (position > 4)
+        {
+            colorIndicator.SetActive(false);
+        }
+        else
+        {
+            colorIndicator.SetActive(true);
+        }
+
         //use formula to determine new X value
         int newXPosition = -500 + (position * 125) + 960;
         colorIndicator.GetComponent<RectTransform>().position = new Vector3(newXPosition,
@@ -75,7 +85,6 @@ public class FactionManager : MonoBehaviour
                 returnInt = FactonColors.IndexOf(color);
             }
         }
-
         return returnInt;
     }
 
